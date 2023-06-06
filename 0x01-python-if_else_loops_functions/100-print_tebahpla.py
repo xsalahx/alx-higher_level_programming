a5355ab8f37384c03d718fff102e73575362334a
Salah <salah.elhssani@gmail.com>
2023-06-06 10:11:10 -0700

    Task 14

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,9 +2,9 @@
 toggle = True
 for i in range(122, 96, -1):
     if toggle == True:
-        print(f"{chr(i)}", end="")
+        print("{}".format(chr(i)), end="")
         toggle = False
     else:
         shift = ord('a') - ord('A')
-        print(f"{chr(i - shift)}", end="")
+        print("{}".format(chr(i - shift)), end="")
         toggle = True
