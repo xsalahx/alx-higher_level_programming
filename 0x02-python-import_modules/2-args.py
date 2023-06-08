72af81390c2333bd4e4db41893c5d5e2a870f45e
Salah <salah.elhssani@gmail.com>
2023-06-08 03:42:20 -0700

    fix pycodestyle

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 import sys
+
+
 if __name__ == "__main__":
-    l = len(sys.argv)
-    print(f"{l - 1} argument", end="")
-    if l == 1:
+    lenght = len(sys.argv)
+    print(f"{lenght - 1} argument", end="")
+    if lenght == 1:
         print(".")
-    elif l == 2:
+    elif lenght == 2:
         print(":")
         print(f"1: {sys.argv[1]}")
     else:
         print("s:")
-        for i in range(1, l):
+        for i in range(1, lenght):
             print(f"{i}: {sys.argv[i]}")
-
