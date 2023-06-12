53d8d1094a7dcc7650de2c03f74fcae3746e4616
Salah <salah.elhssani@gmail.com>
2023-06-12 12:20:25 -0700

    update

---
+++ 0x03-python-data_structures/3-main.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+print_reversed_list_integer = __import__('3-print_reversed_list_integer').print_reversed_list_integer
+
+my_list = ['r', 'a']
+print_reversed_list_integer(my_list)
