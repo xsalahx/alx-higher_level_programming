4bfa09c6c89ea408c3e52991c654e118dd13bc8e
Salah <salah.elhssani@gmail.com>
2023-06-12 12:35:45 -0700

    task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     lst = [0, 0]
-    for i in range(min(len(tuple_a), max(2, len(tuple_a)))):
+    for i in range(min(2, len(tuple_a))):
         lst[i] += tuple_a[i]
-    for i in range(min(len(tuple_b), max(2, len(tuple_b)))):
+    for i in range(min(2, len(tuple_b))):
         lst[i] += tuple_b[i]
     return (lst[0], lst[1])
